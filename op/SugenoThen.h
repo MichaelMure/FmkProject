8e5c5b807df4dce28887a5603b413f4bcc43eeed
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-05-04 14:56:14 +0200

    Correction : ajout de premValue en parametre du constructeur de SugenoThen

--- op/SugenoThen.h
+++ op/SugenoThen.h
@@ -14,11 +14,16 @@ namespace op {
 	template<class T>
 	class SugenoThen : public op::Then<T>{
 	public:
+		SugenoThen(T);
 		virtual T evaluate(expr::Expression<T> * left, expr::Expression<T>* right) const;
 		virtual T premiseValue();
 	private:
-		T premiseValue;
+		T premValue;
 	};
+
+	template<class T>
+	SugenoThen <T>::SugenoThen(T _premiseValue):
+		premValue(_premiseValue) {}
 
 	template<class T>
 	T SugenoThen <T>::evaluate(expr::Expression<T>* left, expr::Expression<T>* right) const
@@ -29,7 +34,7 @@ namespace op {
 	template<class T>
 	T SugenoThen <T>::premiseValue()
 	{
-		return premiseValue;
+		return premValue;
 	}
 }
 #endif /* SUGENOTHEN_H_ */
